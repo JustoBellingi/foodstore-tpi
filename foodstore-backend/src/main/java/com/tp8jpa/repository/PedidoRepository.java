86032267bfa19503814ebf6ab674249d183fef0e
JustoBellingi <jbellingi2006@gmail.com>
2026-06-25 11:25:43 -0300

    mejoras en PedidoRepository

--- foodstore-backend/src/main/java/com/tp8jpa/repository/PedidoRepository.java
+++ foodstore-backend/src/main/java/com/tp8jpa/repository/PedidoRepository.java
@@ -4,6 +4,7 @@ import com.tp8jpa.entities.Pedido;
 import com.tp8jpa.entities.enums.Estado;
 import com.tp8jpa.utils.JPAUtil;
 import jakarta.persistence.EntityManager;
+
 import java.util.List;
 
 public class PedidoRepository extends BaseRepository<Pedido> {
@@ -11,14 +12,11 @@ public class PedidoRepository extends BaseRepository<Pedido> {
     public PedidoRepository() {
         super(Pedido.class);
     }
-
     public List<Pedido> buscarPorUsuario(Long idUsuario) {
 
         EntityManager em = JPAUtil.getEntityManager();
 
         try {
-
-            // Consulta JPQL: retorna todos los pedidos activos de un usuario
 
             String jpql = """
                     SELECT p
@@ -33,19 +31,14 @@ public class PedidoRepository extends BaseRepository<Pedido> {
                     .getResultList();
 
         } finally {
-
             em.close();
-
         }
     }
-
     public List<Pedido> buscarPorEstado(Estado estado) {
 
         EntityManager em = JPAUtil.getEntityManager();
 
         try {
-
-            // Consulta JPQL: retorna todos los pedidos activos con un estado específico
 
             String jpql = """
                     SELECT p
@@ -59,9 +52,40 @@ public class PedidoRepository extends BaseRepository<Pedido> {
                     .getResultList();
 
         } finally {
-
             em.close();
-
         }
+    }
+    public List<Pedido> buscarTerminados() {
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try {
+
+            String jpql = """
+                    SELECT p
+                    FROM Pedido p
+                    WHERE p.estado = :estado
+                    AND p.eliminado = false
+                    """;
+
+            return em.createQuery(jpql, Pedido.class)
+                    .setParameter("estado", Estado.TERMINADO)
+                    .getResultList();
+
+        } finally {
+            em.close();
+        }
+    }
+    public double totalFacturado() {
+
+        List<Pedido> pedidos = buscarTerminados();
+
+        double total = 0;
+
+        for (Pedido p : pedidos) {
+            total += p.getTotal() != null ? p.getTotal() : 0.0;
+        }
+
+        return total;
     }
 }
